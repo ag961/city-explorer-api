453a5661bb7c3958bcdc951c54d76d2f72a3b356
Ayrat Gimranov <gimranov45@gmail.com>
2021-08-02 04:21:57 +0300

    finished lab07

--- server.js
+++ server.js
@@ -3,27 +3,44 @@
 console.log('Hello world');
 
 const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+require('dotenv').config();
+const PORT = process.env.PORT;
+//--------------------------------------------------------------//
 
+class Forecast {
+  constructor(description, date) {
+    this.description = description;
+    this.date = date;
+  }
+}
 const weatherData = require('./data/weather.json');
 
-console.log(weatherData);
-
-const app = express();
 
 app.get('/', (request, response) => {
   response.send('Hello, from the server!');
-
 });
 
 app.get('/weather', (request, response) => {
   let cityNameRequested = request.query.searchQuery;
-  let latRequested = request.query.lat;
-  let lonRequested = request.query.lon;
+  let latRequested = parseInt(request.query.lat);
+  let lonRequested = parseInt(request.query.lon);
+  let cityFound = weatherData.find(obj => obj.city_name.toLowerCase() === cityNameRequested.toLowerCase() && parseInt(obj.lat) === latRequested && parseInt(obj.lon) === lonRequested);
+  let forecastArray = [];
 
-  let cityFound = weatherData.find(obj => obj.city_name.toLowerCase() === cityNameRequested.toLowerCase() && obj.lat === latRequested && obj.lon === lonRequested);
-  console.log(cityFound);
-  response.send(`city is ${cityFound.city_name}`);
+  if (cityFound) {
+    cityFound.data.forEach(obj => forecastArray.push(new Forecast(`Low of ${obj.low_temp}, high of ${obj.high_temp} with ${obj.weather.description}`, obj.datetime)));
+    response.send(forecastArray);
+  } else {
+    response.status(404).send('No weather data exists for this city');
+  }
 });
 
-app.listen(3001, () => console.log('listening on port 3001'));
+app.get('/*', (request, response) => {
+  response.status(500).send('Path does not exists');
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
